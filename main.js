0b736001eac157e23ee649faaf96485b2c3511b7
Gottskalksson <adam.rolirad@gmail.com>
2020-02-16 15:57:57 +0100

    task 2 & css file finished

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 $(function () {
-        // setInterval(function () {
-            $.ajax({
-                method: "GET",
-                url: 'https://swapi.co/api/people/4'
-            }).done(function (result) {
-                console.log(result);
-            });
-        // }, 3000);
+    var app = $("#app");
+
+    function renderBooks (books) {
+        books.forEach(function (book) {
+            var bookElement = $("<div>");
+            bookElement.text(book.title).appendTo(app);
+
+            bookElement
+                .addClass("book-title")
+                .text(book.title)
+                .appendTo(app);
+
+            var bookTitle = $("<div>");
+
+            bookTitle
+                .addClass("book-title")
+                .text(book.title)
+                .appendTo(bookElement);
+
+            var bookDetails = $("<div>");
+            bookDetails
+                .addClass("book-deatils")
+                .appendTo(bookElement);
+        });
+    }
+
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:8282/books"
+    }).done(renderBooks);
 })
 
