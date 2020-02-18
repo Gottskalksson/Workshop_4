b1653e6b3c361cf22b4c07af00a0d87b030f6bb1
Gottskalksson <adam.rolirad@gmail.com>
2020-02-18 11:59:30 +0100

    task 6 done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-$(function() {
+$(function () {
     var app = $("#app");
+
     function apiCall(method, path, data) {
         var settings = {
             method: method,
@@ -11,6 +12,7 @@ $(function() {
         }
         return $.ajax(settings);
     }
+
     function renderBookDetails(bookDetails, data) {
         bookDetails
             .empty()
@@ -21,42 +23,53 @@ $(function() {
             .append("Wydawca: " + data.publisher + "<br />")
             .append("Typ: " + data.type + "<br />");
     }
+
     function renderBooks(books) {
         app.empty();
-        books.forEach(function(book) {
+        books.forEach(function (book) {
             var bookElement = $("<div>");
             var bookTitle = $("<div>");
             var bookDetails = $("<div>");
+            var link = $("<div>");
             bookElement.addClass("book").appendTo(app);
             bookTitle
                 .addClass("book-title")
                 .text(book.title)
                 .appendTo(bookElement);
-            bookTitle.one("click", function() {
+            bookTitle.one("click", function () {
                 bookDetails.text("...");
-                apiCall("GET", "/books/" + book.id).done(function(data) {
+                apiCall("GET", "/books/" + book.id).done(function (data) {
                     renderBookDetails(bookDetails, data);
                 });
             });
             bookDetails.addClass("book-details").appendTo(bookElement);
+            link.addClass("delete")
+                .text("Usuń książkę")
+                .appendTo(bookElement);
+            link.on("click", function () {
+               apiCall("DELETE", "/books/" + book.id).done(fetchBooks);
+            });
         });
     }
+
     function fetchBooks() {
         apiCall("GET", "/books").done(renderBooks);
     }
+
     function handleForm() {
-        $("#bookForm").on("submit", function(e) {
+        $("#bookForm").on("submit", function (e) {
             e.preventDefault();
             var data = {};
             $(this)
                 .find("input")
-                .each(function() {
+                .each(function () {
                     var name = $(this).attr("name");
                     data[name] = $(this).val();
                 });
             apiCall("POST", "/books", data).done(fetchBooks);
         });
     }
+
     fetchBooks();
     handleForm();
 });
